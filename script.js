b36b24a1c26c010350c6a361792a9c421d0bec9a
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-04-01 20:58:10 +0200

    Convert from browser-based rendering to Jinja2 templates
    This will make it possible for search engines to index the generated
    HTML.

--- script.js
+++ script.js
@@ -11,120 +11,10 @@ const MESSAGES = {
 
 var data;
 
-function hashchange(e) {
-  if (e) {
-    document.body.classList.toggle('hide-nav', true); // disable nav on mobile
-  }
-
-  var nav = document.querySelector('#nav');
-  var main = document.querySelector('#main');
-  var hash = document.location.hash;
-  if (!hash) {
-    hash = '#'; // for consistency
-  }
-
-  $('#tree .nav-item a.active').removeClass('active');
-  $('#tree .nav-item a[href="' + hash + '"]').addClass('active');
-
-  var parts = hash.substr(1).split('.');
-  if (hash === '#') {
-    main.innerHTML = document.querySelector('#templates > .page-start').innerHTML;
-    var schemeIds = Object.keys(data);
-    schemeIds.sort();
-    for (var schemeId of schemeIds) {
-      var schemeData = data[schemeId];
-      main.querySelector('.schememanagers').innerHTML += document.querySelector('#templates > .fragment-schememanager').innerHTML;
-      var schememanager = main.querySelector('.schememanagers .schememanager:last-child');
-      schememanager.querySelector('.name').innerText = schemeData.name[LANG];
-      schememanager.querySelector('.name').setAttribute('href', '#' + schemeId);
-      schememanager.querySelector('.description').innerText = schemeData.description[LANG];
-    }
-  } else if (parts.length == 2) {
-    main.innerHTML = document.querySelector('#templates > .page-schememanager').innerHTML;
-    var schemeId = parts[0];
-    var issuerId = parts[1];
-    var schemeManager = data[schemeId];
-    var issuer = schemeManager.issuers[issuerId];
-    main.querySelector('.name').innerText = issuer.name[LANG];
-    main.querySelector('.logo').setAttribute('src', issuer.logo);
-    main.querySelector('.path').innerText = schemeId + '.' + issuerId;
-    main.querySelector('.description').innerText = schemeManager.description[LANG];
-    main.querySelector('.shortName').innerText = issuer.shortName[LANG];
-    main.querySelector('.contact a').innerText = schemeManager.contact;
-    main.querySelector('.contact a').setAttribute('href', schemeManager.contact);
-    main.querySelector('.contactEmail a').innerText = issuer.contactEmail;
-    main.querySelector('.contactEmail a').setAttribute('href', 'mailto:' + issuer.contactEmail);
-    main.querySelector('.url').innerText = schemeManager.url;
-    var credentialIds = Object.keys(issuer.credentials);
-    credentialIds.sort();
-    for (var credentialId of credentialIds) {
-      var credentialData = issuer.credentials[credentialId];
-      main.querySelector('.credentials').innerHTML += document.querySelector('#templates > .fragment-credential').innerHTML;
-      var credential = main.querySelector('.credentials .credential:last-child');
-      credential.querySelector('.name').innerText = credentialData.name[LANG];
-      credential.querySelector('.name').setAttribute('href', '#' + schemeId + '.' + issuerId + '.' + credentialId);
-      credential.querySelector('.description').innerText = credentialData.description[LANG];
-    }
-  } else if (parts.length == 3) {
-    main.innerHTML = document.querySelector('#templates > .page-credential').innerHTML;
-    var schemeId = parts[0];
-    var issuerId = parts[1];
-    var credentialId = parts[2];
-    var credential = data[schemeId].issuers[issuerId].credentials[credentialId];
-    main.querySelector('.name').innerText = credential.name[LANG];
-    main.querySelector('.path').innerText = parts.join('.');
-    main.querySelector('.logo').setAttribute('src', credential.logo);
-    main.querySelector('.description').innerText = credential.description[LANG];
-    main.querySelector('.shortName').innerText = credential.shortName[LANG];
-    main.querySelector('.singleton').innerText = MESSAGES[credential.shouldBeSingleton ? 'yes' : 'no'];
-    for (var attribute of credential.attributes) {
-      main.querySelector('.attributes').innerHTML += document.querySelector('#templates > .fragment-attribute').innerHTML;
-      var element = main.querySelector('.attribute:last-child');
-      element.querySelector('.name').innerText = attribute.name[LANG];
-      element.querySelector('.path').innerText = parts.join('.') + '.' + attribute.id;
-      element.querySelector('.description').innerText = attribute.description[LANG];
-    }
-  } else {
-    console.error('unknown hash:', hash);
-    main.innerHTML = ''; // TODO: show 404 or something
-  }
-}
-
-function buildMenuTree() {
-  var tree = document.querySelector('#tree');
-  tree.appendChild(document.querySelector('#templates > .treenode-about').children[0].cloneNode(true));
-  for (var schemeId in data) {
-    var schemeData = data[schemeId];
-    for (var issuerId in schemeData.issuers) {
-      var issuerData = schemeData.issuers[issuerId];
-      var rootItem = document.querySelector('#templates > .treenode-schememanager').children[0].cloneNode(true);
-      tree.appendChild(rootItem);
-      var rootEl = rootItem.querySelector('.nav-link');
-      rootEl.innerText = issuerData.shortName[LANG];
-      rootEl.setAttribute('title', issuerData.name[LANG]);
-      rootEl.setAttribute('href', '#' + schemeId + '.' + issuerId);
-      var ids = Object.keys(issuerData.credentials);
-      ids.sort();
-      for (var credentialId of ids) {
-        var credentialData = issuerData.credentials[credentialId];
-        rootItem.innerHTML += document.querySelector('#templates > .treenode-credential').innerHTML;
-        var credentialEl = rootItem.querySelector('.credential:last-child');
-        credentialEl.querySelector('a').innerText = credentialData.shortName[LANG];
-        credentialEl.querySelector('a').setAttribute('title', credentialData.name[LANG]);
-        credentialEl.querySelector('a').setAttribute('href', '#' + schemeId + '.' + issuerId + '.' + credentialId);
-      }
-    }
-  }
-  //$('[data-toggle="tooltip"]', tree).tooltip()
-  hashchange();
-  window.onhashchange = hashchange;
-}
-
 function doSearch(e) {
   var string = e.target.value;
-  $('#tree > li:not(.search)').remove();
+  document.body.classList.toggle('in-search', string.length != 0);
   if (!string) {
-    buildMenuTree();
     return;
   }
 
@@ -134,18 +24,19 @@ function doSearch(e) {
 function init() {
   document.querySelector('.toggle-nav').onclick = function(e) {
     e.preventDefault();
-    document.body.classList.toggle('hide-nav');
+    document.body.classList.toggle('show-nav');
   };
 
   document.querySelector('.search').oninput = doSearch;
 
   var req = new XMLHttpRequest();
-  req.open('GET', 'data.json');
+  req.open('GET', INDEX_URL);
   req.send();
   req.onload = function(e) {
     data = JSON.parse(req.responseText);
-    buildMenuTree();
   }
+
+  //$('[data-toggle="tooltip"]', tree).tooltip()
 }
 
 init();
--- update.py
+++ update.py
@@ -3,11 +3,26 @@
 import json
 from xml.dom import minidom
 import os
+import jinja2 # Debian: python3-jinja2
 
 BOOL = {
-    'true': True,
+    'true':  True,
     'false': False,
 }
+
+def filter_active(highlightpath, currentpath):
+    if highlightpath == currentpath:
+        return 'active'
+
+def render(out, template_name, **kwargs):
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('templates'),
+        autoescape=True)
+    env.filters['active'] = filter_active
+    template = env.get_template(template_name)
+    html = template.render(base='.', assets='..', **kwargs)
+    open(out, 'w').write(html)
+
 
 def getText(element):
     strings = []
@@ -24,9 +39,21 @@ def translated(element):
         strings[child.nodeName] = getText(child)
     return strings
 
+def readAttribute(xml, credential):
+    attribute = {
+        'id':          xml.getAttribute('id'),
+        'name':        translated(xml.getElementsByTagName('Name')[0]),
+        'description': translated(xml.getElementsByTagName('Description')[0]),
+    }
+    attribute['path'] = '%s.%s' % (credential['path'], attribute['id'])
+    return attribute
+
 def readCredential(path):
     xml = minidom.parse(path + '/description.xml')
     credential = {
+        'schememgr':         getText(xml.getElementsByTagName('SchemeManager')[0]),
+        'issuer':            getText(xml.getElementsByTagName('IssuerID')[0]),
+        'id':                getText(xml.getElementsByTagName('CredentialID')[0]),
         'name':              translated(xml.getElementsByTagName('Name')[0]),
         'shortName':         translated(xml.getElementsByTagName('ShortName')[0]),
         'description':       translated(xml.getElementsByTagName('Description')[0]),
@@ -34,6 +61,7 @@ def readCredential(path):
         'shouldBeSingleton': False, # default?
         'attributes':        [],
     }
+    credential['path'] = '%s.%s.%s' % (credential['schememgr'], credential['issuer'], credential['id'])
 
     singletonElements = xml.getElementsByTagName('ShouldBeSingleton')
     if singletonElements:
@@ -43,35 +71,34 @@ def readCredential(path):
     for attribute in xml.getElementsByTagName('Attributes')[0].childNodes:
         if attribute.nodeType != attribute.ELEMENT_NODE:
             continue
-        credential['attributes'].append({
-            'id':          attribute.getAttribute('id'),
-            'name':        translated(attribute.getElementsByTagName('Name')[0]),
-            'description': translated(attribute.getElementsByTagName('Description')[0]),
-        })
+        credential['attributes'].append(readAttribute(attribute, credential))
 
     return credential
 
-def readSchemeManager(path):
+def readIssuer(path):
     xml = minidom.parse(path + '/description.xml')
-    index = {
+    issuer = {
+        'id':           getText(xml.getElementsByTagName('ID')[0]),
+        'schememgr':    getText(xml.getElementsByTagName('SchemeManager')[0]),
         'shortName':    translated(xml.getElementsByTagName('ShortName')[0]),
         'name':         translated(xml.getElementsByTagName('Name')[0]),
         'contactEmail': getText(xml.getElementsByTagName('ContactEMail')[0]),
         'logo':         path + '/logo.png',
         'credentials':  {},
     }
+    issuer['path'] = '%s.%s' % (issuer['schememgr'], issuer['id'])
 
     for fn in sorted(os.listdir(path + '/Issues')):
-        index['credentials'][fn] = readCredential(path + '/Issues/' + fn)
+        issuer['credentials'][fn] = readCredential(path + '/Issues/' + fn)
 
-    return index
+    return issuer
 
-def makeIndex(path):
+def readSchemeManager(path):
     index = {}
 
     xml = minidom.parse(path + '/description.xml')
-    id = getText(xml.getElementsByTagName('Id')[0])
     index = {
+        'id':          getText(xml.getElementsByTagName('Id')[0]),
         'name':        translated(xml.getElementsByTagName('Name')[0]),
         'description': translated(xml.getElementsByTagName('Description')[0]),
         'url':         getText(xml.getElementsByTagName('Url')[0]),
@@ -80,17 +107,42 @@ def makeIndex(path):
     }
 
     for fn in sorted(os.listdir(path)):
-        schemeManagerPath = path + '/' + fn
-        if os.path.exists(schemeManagerPath + '/description.xml'):
-            index['issuers'][fn] = readSchemeManager(schemeManagerPath)
+        issuerPath = path + '/' + fn
+        if os.path.exists(issuerPath + '/description.xml'):
+            index['issuers'][fn] = readIssuer(issuerPath)
 
-    return id, index
+    return index
+
+
+def generateHTML(index, out, lang):
+    os.makedirs(out, exist_ok=True)
+    render(out + '/index.html', 'about.html',
+           index=index,
+           LANG=lang,
+           path='')
+
+    for schememgr in index:
+        for issuerId, issuer in sorted(schememgr['issuers'].items()):
+            render(out + '/' + issuer['path'] + '.html', 'issuer.html',
+                   index=index,
+                   schememgr=schememgr,
+                   issuer=issuer,
+                   LANG=lang,
+                   path=issuer['path'])
+            for credentialId, credential in sorted(issuer['credentials'].items()):
+                render(out + '/' + credential['path'] + '.html', 'credential.html',
+                       index=index,
+                       schememgr=schememgr,
+                       issuer=issuer,
+                       credential=credential,
+                       LANG=lang,
+                       path=credential['path'])
 
 
 if __name__ == '__main__':
     schememanagers = ['pbdf-schememanager', 'irma-demo-schememanager']
-    index = {}
+    index = []
     for path in schememanagers:
-        id, data = makeIndex(path)
-        index[id] = data
+        index.append(readSchemeManager(path))
     json.dump(index, open('data.json', 'w'))
+    generateHTML(index, 'en', 'en')
